f9ad8feac300eb0c363a1cf351443020494e6711
twolak88 <twolak88@gmail.com>
2020-08-09 15:55:03 +0200

    Initial Project add

---
+++ src/main/java/twolak/springframework/twspringpetclinic/TwSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package twolak.springframework.twspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwSpringPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwSpringPetClinicApplication.class, args);
+	}
+
+}
