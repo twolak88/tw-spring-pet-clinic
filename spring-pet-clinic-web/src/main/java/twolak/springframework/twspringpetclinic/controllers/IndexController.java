5da93468f17e9af1949181282cc50e6d356452f0
twolak88 <twolak88@gmail.com>
2020-08-11 23:50:17 +0200

    added index page and controller. closes #8

---
+++ spring-pet-clinic-web/src/main/java/twolak/springframework/twspringpetclinic/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package twolak.springframework.twspringpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author twolak
+ *
+ */
+@Controller
+public class IndexController {
+    
+    @GetMapping({"", "/", "index", "index.html"})
+    public String index() {
+	return "index";
+    }
+
+}
