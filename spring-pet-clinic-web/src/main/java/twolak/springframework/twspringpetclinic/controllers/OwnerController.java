b2451e1f30d0adf80026542018cecda5d92de146
twolak88 <twolak88@gmail.com>
2020-08-14 14:01:30 +0200

    Added vet list view. closes #15.

--- spring-pet-clinic-web/src/main/java/twolak/springframework/twspringpetclinic/controllers/OwnerController.java
+++ spring-pet-clinic-web/src/main/java/twolak/springframework/twspringpetclinic/controllers/OwnerController.java
@@ -1,8 +1,11 @@
 package twolak.springframework.twspringpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import twolak.springframework.twspringpetclinic.services.OwnerService;
 
 /**
  * @author twolak
@@ -11,9 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
-    
-    @GetMapping({"", "index", "index.html"})
-    public String listOwners() {
+
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+	this.ownerService = ownerService;
+    }
+
+    @GetMapping({ "", "index", "index.html" })
+    public String listOwners(Model model) {
+
+	model.addAttribute("owners", ownerService.findAll());
+
 	return "owners/index";
     }
 
--- spring-pet-clinic-web/src/main/java/twolak/springframework/twspringpetclinic/controllers/VetController.java
+++ spring-pet-clinic-web/src/main/java/twolak/springframework/twspringpetclinic/controllers/VetController.java
@@ -4,8 +4,11 @@
 package twolak.springframework.twspringpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import twolak.springframework.twspringpetclinic.services.VetService;
 
 /**
  * @author twolak
@@ -15,8 +18,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class VetController {
     
+    private final VetService vetService;
+    
+    public VetController(VetService vetService) {
+	this.vetService = vetService;
+    }
+
     @GetMapping({"", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+	
+	model.addAttribute("vets", vetService.findAll());
+	
 	return "vets/index";
     }
 }
