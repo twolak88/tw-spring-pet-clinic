757806ac71ed698a5b11f04f599ffe8e78a24621
twolak88 <twolak88@gmail.com>
2020-08-16 20:47:09 +0200

    added Pet Type Service. closes #29

---
+++ spring-pet-clinic-data/src/main/java/twolak/springframework/twspringpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package twolak.springframework.twspringpetclinic.services;
+
+import twolak.springframework.twspringpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
---
+++ spring-pet-clinic-data/src/main/java/twolak/springframework/twspringpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,42 @@
+package twolak.springframework.twspringpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import twolak.springframework.twspringpetclinic.model.PetType;
+import twolak.springframework.twspringpetclinic.services.PetTypeService;
+
+/**
+ * @author twolak
+ *
+ */
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+	return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+	return super.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+	return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+	super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+	super.delete(object);
+    }
+
+}
