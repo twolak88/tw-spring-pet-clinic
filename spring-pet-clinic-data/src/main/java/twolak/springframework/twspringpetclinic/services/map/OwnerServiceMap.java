c50338e8a97ad186a827cacf8396a0e1aa28790a
twolak88 <twolak88@gmail.com>
2020-08-16 23:32:16 +0200

    Add Pets and Contacts to Owner on startup. closes #33

--- spring-pet-clinic-data/src/main/java/twolak/springframework/twspringpetclinic/services/map/OwnerServiceMap.java
+++ spring-pet-clinic-data/src/main/java/twolak/springframework/twspringpetclinic/services/map/OwnerServiceMap.java
@@ -5,7 +5,11 @@ import java.util.Set;
 import org.springframework.stereotype.Service;
 
 import twolak.springframework.twspringpetclinic.model.Owner;
+import twolak.springframework.twspringpetclinic.model.Pet;
+import twolak.springframework.twspringpetclinic.model.PetType;
 import twolak.springframework.twspringpetclinic.services.OwnerService;
+import twolak.springframework.twspringpetclinic.services.PetService;
+import twolak.springframework.twspringpetclinic.services.PetTypeService;
 
 /**
  * @author twolak
@@ -14,6 +18,14 @@ import twolak.springframework.twspringpetclinic.services.OwnerService;
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+    
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+	this.petTypeService = petTypeService;
+	this.petService = petService;
+    }
+
     @Override
     public Set<Owner> findAll() {
 	return super.findAll();
@@ -26,7 +38,28 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
     
     @Override
     public Owner save(Owner object) {
-	return super.save(object);
+	if (object != null) {
+	    if (object.getPets() != null) {
+		object.getPets().forEach(pet-> {
+		    if (pet.getPetType() != null) {
+			if (pet.getPetType().getId() == null) {
+			    pet.setPetType(petTypeService.save(pet.getPetType()));
+			}
+		    } else {
+			throw new RuntimeException("Pet Type is required");
+		    }
+		    
+		    if (pet.getId() == null) {
+			Pet savedPet = petService.save(pet);
+			pet.setId(savedPet.getId());
+		    }
+		});
+	    }
+	    return super.save(object);
+	} else {
+	    return null;
+	}
+	
     }
     
     @Override
--- spring-pet-clinic-web/src/main/java/twolak/springframework/twspringpetclinic/bootstrap/DataLoader.java
+++ spring-pet-clinic-web/src/main/java/twolak/springframework/twspringpetclinic/bootstrap/DataLoader.java
@@ -1,9 +1,14 @@
 package twolak.springframework.twspringpetclinic.bootstrap;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import twolak.springframework.twspringpetclinic.model.Owner;
+import twolak.springframework.twspringpetclinic.model.Pet;
 import twolak.springframework.twspringpetclinic.model.PetType;
 import twolak.springframework.twspringpetclinic.model.Vet;
 import twolak.springframework.twspringpetclinic.services.OwnerService;
@@ -29,27 +34,31 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-	createPets();
-	createOwners();
-	createVets();
-    }
-
-    private void createPets() {
+	
 	PetType dogPetType = createPetType("Dog");
 	PetType catPetType = createPetType("Cat");
 	PetType birdPetType = createPetType("Bird");
-	
+
 	System.out.println("PetTypes loaded...");
-    }
+	
+	Owner tom = createOwner("Thomas", "Smith", "23 Perl", "Chicago", "342234567");
+	tom.getPets().add(createPet(LocalDate.now().plusDays(-12), "Coco", birdPetType, tom));
+	tom.getPets().add(createPet(LocalDate.now().plusDays(-543), "Dragon", dogPetType, tom));
+	ownerService.save(tom);
+	
+	Owner mike = createOwner("Michael", "Weston", "123 Brickerel", "Miami", "321456789");
+	mike.getPets().add(createPet(LocalDate.now().plusDays(-123), "Pluto", dogPetType, mike));
+	mike.getPets().add(createPet(LocalDate.now().plusDays(-764), "Guffi", dogPetType, mike));
+	ownerService.save(mike);
+	
+	Owner fiona = createOwner("Fiona", "Glenanne", "23 Brickerel", "Miami", "765567898");
+	fiona.getPets().add(createPet(LocalDate.now().plusDays(-432), "Luna", catPetType, fiona));
+	fiona.getPets().add(createPet(LocalDate.now().plusDays(-888), "Maya", dogPetType, fiona));
+	fiona.getPets().add(createPet(LocalDate.now().plusDays(-100), "Arnold", birdPetType, fiona));
+	ownerService.save(fiona);
 
-    private PetType createPetType(String name) {
-	PetType petType = new PetType();
-	petType.setName(name);
-
-	return petTypeService.save(petType);
-    }
-
-    private void createVets() {
+	System.out.println("Owners with pets loaded...");
+	
 	createVet("Patrick", "Jane");
 	createVet("Sam", "Axe");
 	createVet("Kurt", "Wild");
@@ -57,27 +66,37 @@ public class DataLoader implements CommandLineRunner {
 	System.out.println("Vets loaded...");
     }
 
+    private Pet createPet(LocalDate birthDate, String name, PetType petType, Owner owner) {
+	Pet pet = new Pet();
+	pet.setName(name);
+	pet.setPetType(petType);
+	pet.setBirthDate(birthDate);
+	pet.setOwner(owner);
+	return pet;
+    }
+
+    private PetType createPetType(String name) {
+	PetType petType = new PetType();
+	petType.setName(name);
+
+	return petType;
+    }
+
     private void createVet(String firstName, String lastName) {
-	Vet vet1 = new Vet();
-	vet1.setFirstName(firstName);
-	vet1.setLastName(lastName);
+	Vet vet = new Vet();
+	vet.setFirstName(firstName);
+	vet.setLastName(lastName);
 
-	vetService.save(vet1);
+	vetService.save(vet);
     }
 
-    private void createOwners() {
-	createOwner("Thomas", "Smith");
-	createOwner("Michael", "Weston");
-	createOwner("Fiona", "Glenanne");
-
-	System.out.println("Owners loaded...");
-    }
-
-    private void createOwner(String firstName, String lastName) {
-	Owner owner1 = new Owner();
-	owner1.setFirstName(firstName);
-	owner1.setLastName(lastName);
-
-	ownerService.save(owner1);
+    private Owner createOwner(String firstName, String lastName, String address, String city, String phone) {
+	Owner owner = new Owner();
+	owner.setFirstName(firstName);
+	owner.setLastName(lastName);
+	owner.setAddress(address);
+	owner.setCity(city);
+	owner.setPhone(phone);
+	return owner;
     }
 }
