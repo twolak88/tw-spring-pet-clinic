1c614cb07eb8293290aa4c121fd01b7ee6ec19e8
twolak88 <twolak88@gmail.com>
2020-08-19 00:30:08 +0200

    added Visit Entity. Closes #38.

--- spring-pet-clinic-data/src/main/java/twolak/springframework/twspringpetclinic/model/Visit.java
+++ spring-pet-clinic-data/src/main/java/twolak/springframework/twspringpetclinic/model/Visit.java
@@ -2,14 +2,31 @@ package twolak.springframework.twspringpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 /**
  * @author twolak
  *
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
     
+	@Column(name = "date")
+	@CreationTimestamp
     private LocalDate date;
+	
+	@Column(name = "description")
     private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
     private Pet pet;
     
     public LocalDate getDate() {
