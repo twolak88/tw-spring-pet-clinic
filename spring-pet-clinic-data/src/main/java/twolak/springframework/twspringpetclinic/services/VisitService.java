2e8cf907b65c3df50e170b6b4a43224f14603f60
twolak88 <twolak88@gmail.com>
2020-08-19 19:40:30 +0200

    Added Visit Map Service closes #46

---
+++ spring-pet-clinic-data/src/main/java/twolak/springframework/twspringpetclinic/services/VisitService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package twolak.springframework.twspringpetclinic.services;
+
+import twolak.springframework.twspringpetclinic.model.Visit;
+
+/**
+ * @author twolak
+ *
+ */
+public interface VisitService extends CrudService<Visit, Long>{
+
+}
