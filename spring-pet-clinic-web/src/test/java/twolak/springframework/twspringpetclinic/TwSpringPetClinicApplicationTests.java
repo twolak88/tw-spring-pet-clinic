114a1677e89281355b38921b0ea9c9d3ca30925d
twolak88 <twolak88@gmail.com>
2020-08-21 20:47:44 +0200

    Integration test in Junit 5. closes #4

--- spring-pet-clinic-web/src/test/java/twolak/springframework/twspringpetclinic/TwSpringPetClinicApplicationTests.java
+++ spring-pet-clinic-web/src/test/java/twolak/springframework/twspringpetclinic/TwSpringPetClinicApplicationTests.java
@@ -1,11 +1,14 @@
 package twolak.springframework.twspringpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class TwSpringPetClinicApplicationTests {
-
+	
 	@Test
 	void contextLoads() {
 	}
