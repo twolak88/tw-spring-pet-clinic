20d7582e3dcfc45eea8db99e77a328012aae14af
twolak88 <twolak88@gmail.com>
2020-08-31 20:09:56 +0200

    fix display owner details. Closes #62

--- spring-pet-clinic-data/src/main/java/twolak/springframework/twspringpetclinic/model/PetType.java
+++ spring-pet-clinic-data/src/main/java/twolak/springframework/twspringpetclinic/model/PetType.java
@@ -9,6 +9,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 /**
  * @author twolak
@@ -18,6 +19,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+//@ToString(includeFieldNames=false)
 @Builder
 @Entity
 @Table(name = "types")
@@ -25,4 +27,9 @@ public class PetType extends BaseEntity {
 	
 	@Column(name = "name")
 	private String name;
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
 }
